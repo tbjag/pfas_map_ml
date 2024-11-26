0daebb33481a6d3feb86a499a05047df91d80578
tbjag <demonburritos@gmail.com>
2024-11-25 19:49:05 -0800

    supervive

--- src/dataset/tiff_to_tensor.py
+++ src/dataset/tiff_to_tensor.py
@@ -5,7 +5,10 @@ from rasterio.mask import mask
 import os
 import numpy as np
 
-def tiff_to_tensor(tiff_file, grid_size, output_directory):
+def tiff_to_tensor(tiff_file, grid_size, output_directory, batch_size):
+    cnt = 1
+    batch_cnt = 0
+
     # Load the TIFF file
     with rasterio.open(tiff_file) as src:
         tiff_data = src.read()
@@ -21,7 +24,17 @@ def tiff_to_tensor(tiff_file, grid_size, output_directory):
                     if not np.any(np.isnan(cell)) or not np.any(cell < 0):
                         cell -= 1
                         cells.append(cell)
- 
+
+                        if cnt == batch_size:
+                            tensors = [torch.tensor(cell, dtype=torch.float32) for cell in cells]
+                            stacked_tensor = torch.stack(tensors)
+                            output_file = os.path.join(output_directory, f'{batch_cnt:03}.pth')
+                            torch.save(stacked_tensor, output_file)
+                            batch_cnt += 1
+                            cells = []
+                            cnt = 0
+
+                        cnt += 1
 
     if cells:
         tensors = [torch.tensor(cell, dtype=torch.float32) for cell in cells]
@@ -43,16 +56,20 @@ def process_directory(directory, grid_size, output_directory):
         for file in files:
             if file.endswith('.tif') or file.endswith('.tiff'):
                 tiff_file = os.path.join(root, file)
+                base_name = os.path.splitext(os.path.basename(tiff_file))[0]
+                out_dir = os.path.join(output_directory, base_name)
+                os.mkdir(out_dir)
+                print(out_dir)
                 try:
-                    tiff_to_tensor(tiff_file, grid_size, output_directory)
+                    tiff_to_tensor(tiff_file, grid_size, out_dir, 1)
                 except Exception as e:
                     print(f"Failed to process {tiff_file}: {e}")
     print('finished')
     
 def main():
-    input_dir = '../../data/temp_'
-    grid_size = 10
-    output_dir = '../../data/target_pth'
+    input_dir = '../../data/tiff_temp'
+    grid_size = 32
+    output_dir = '../../data/large_files_train'
     
     process_directory(input_dir, grid_size, output_dir)
 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import torch
-print(f"CUDA available: {torch.cuda.is_available()}")
-print(f"Current device: {torch.cuda.current_device()}")
-print(f"Device name: {torch.cuda.get_device_name()}")
+
+x = torch.load('data/final_target/0000002.pth')
+
+print(x.shape)
