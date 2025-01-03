69622cb7a8879915a15cca7223678a7c28993421
Rohan Jayasekara <rohanjay321@gmail.com>
2025-01-02 17:37:12 -0800

    unet

--- src/unet.py
+++ src/unet.py
@@ -7,19 +7,16 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.bilinear = False
-        bilinear = False
 
         self.inc = (DoubleConv(n_channels, 64))
         self.down1 = (Down(64, 128))
         self.down2 = (Down(128, 256))
         self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
+        self.down4 = (Down(512, 1024))
+        self.up1 = (Up(1024, 512))
+        self.up2 = (Up(512, 256))
+        self.up3 = (Up(256, 128))
+        self.up4 = (Up(128, 64))
         self.outc = (OutConv(64, n_classes))
 
     def forward(self, x):
@@ -32,8 +29,8 @@ class UNet(nn.Module):
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        x = self.outc(x)
+        return x
 # Define the UNet model
 # class UNet(nn.Module):
 #     def conv_block(self, input, in_c, out_c):
